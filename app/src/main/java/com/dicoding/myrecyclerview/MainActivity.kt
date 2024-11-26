d7ffd2d3943aa0618e3b3a8dd3aa785439b3d83d
Bajindul <ukisaw296@gmail.com>
2024-11-26 13:25:44 +0700

    Latihan RecyclerView

---
+++ app/src/main/java/com/dicoding/myrecyclerview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.dicoding.myrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private val list = ArrayList<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resour
+    }
+}
